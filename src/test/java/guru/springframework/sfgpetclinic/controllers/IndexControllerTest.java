e9856fcba2ccbcb72f34483bbf03174ba9ee62a5
deligkarisk <deligkarisk@gmail.com>
2021-09-12 13:29:51 +0900

    Timeouts

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IndexControllerTest {
@@ -28,5 +30,23 @@ class IndexControllerTest {
         assertThrows(ValueNotFoundException.class, () -> {
             controller.oupsHandler();}
         );
+    }
+
+    @Test
+    @Disabled("demo of timeout")
+    void testTimeout() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(2000);
+        });
+        System.out.println("Execution finished");
+    }
+
+    @Test
+    @Disabled("demo of preemptive timeout")
+    void testTimeoutPreemptively() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(2000);
+        });
+        System.out.println("Execution not reaching this point.");
     }
 }
