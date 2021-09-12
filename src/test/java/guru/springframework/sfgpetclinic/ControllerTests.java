566bda8f4833ba95a9ea7326ccef437aac6ab389
deligkarisk <deligkarisk@gmail.com>
2021-09-13 08:26:54 +0900

    after/before all methods

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -1,8 +1,22 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("Controllers")
 public interface ControllerTests {
 
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Let's do something here");
+    }
+
+
+    @AfterAll
+    default void afterAll() {
+        System.out.println("We did something earlier.");
+    }
 }
