4e7231c4935a8cb25797f8122ffc7b3b8f9fc06f
deligkarisk <deligkarisk@gmail.com>
2021-09-13 08:38:43 +0900

    test info dependency injection

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,7 +18,13 @@ class PersonTest {
                 () -> assertEquals("Buck",person.getLastName()));
     }
 
-    @Test
+    @RepeatedTest(value = 5, name = "{displayName}")
+    @DisplayName("My other repeated test")
+    void myRepeatedTestwithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + " " + repetitionInfo.getCurrentRepetition());
+
+    }
+
     @RepeatedTest(value = 10, name = "{displayName} - {currentRepetition}: {totalRepetitions}")
     @DisplayName("My repeated test")
     void myRepeatedTest() {
