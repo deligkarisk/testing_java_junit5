faf28c1865f3bb99f56a440ba8fa88e771f8eb96
deligkarisk <deligkarisk@gmail.com>
2021-09-13 10:15:56 +0900

    parameterized tests

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,10 +1,46 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OwnerTest {
+
+    @ParameterizedTest(name = "{displayName}[{index}] {arguments}")
+    @ValueSource(strings = {"Spring", "is", "great"})
+    @DisplayName("Value Source Test - ")
+    void testValueSource(String val) {
+        System.out.println(val);
+    }
+
+    @ParameterizedTest(name = "{displayName}[{index}] {arguments}")
+    @EnumSource(OwnerType.class)
+    void enumParameterizedTest(OwnerType ownerType) {
+        System.out.println(ownerType);
+    }
+
+    @ParameterizedTest(name = "{displayName}[{index}] {arguments}")
+    @DisplayName("Value Source Test - ")
+    @CsvSource({
+
+    })
+    void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println("State is: " + stateName + " with val 2: " + val2);
+
+    }
+
+    @ParameterizedTest(name = "{displayName}[{index}] {arguments}")
+    @DisplayName("Value Source Test - ")
+    @CsvFileSource(resources = "/sources.csv", numLinesToSkip = 1)
+    void csvFromFileTest(String stateName, int val1, int val2) {
+        System.out.println("State is: " + stateName + " with val 2: " + val2);
+    }
 
     @Test
     void dependentAssertions() {
