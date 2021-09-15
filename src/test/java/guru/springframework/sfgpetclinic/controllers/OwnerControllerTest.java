dc1ec0f5ce89df3f5014705aa31a43eafa270f93
deligkarisk <deligkarisk@gmail.com>
2021-09-16 08:41:10 +0900

    Testing order of interactions

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -1,23 +1,22 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
+import guru.springframework.sfgpetclinic.fauxspring.Model;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
@@ -37,6 +36,9 @@ class OwnerControllerTest {
     @Mock
     Owner mockOwner;
 
+    @Mock
+    Model model;
+
 
     @Test
     void processCreationFormHasErrors() {
@@ -49,6 +51,26 @@ class OwnerControllerTest {
 
         // then
         assertEquals(OWNERS_CREATE_OR_UPDATE_OWNER_FORM, viewReturned);
+
+    }
+
+    @Test
+    void testOrderOfInteractions() {
+        // given
+        InOrder inOrder = Mockito.inOrder(ownerService, model);
+        Owner owner1 = new Owner(2L, "John", "Doe");
+        List<Owner> listOwners = new ArrayList<>();
+        listOwners.add(owner1);
+        listOwners.add(owner1);
+        given(ownerService.findAllByLastNameLike(anyString())).willReturn(listOwners);
+
+        // when
+        String view = ownerController.processFindForm(mockOwner, bindingResult, model);
+
+        // then
+        inOrder.verify(ownerService).findAllByLastNameLike(anyString());
+        inOrder.verify(model).addAttribute(anyString(), anyList());
+        verifyNoMoreInteractions(model);
 
     }
 
@@ -66,6 +88,7 @@ class OwnerControllerTest {
 
         // then
         assertEquals("%Buck%", captor.getValue());
+        verifyNoMoreInteractions(model);
     }
 
     @Test
